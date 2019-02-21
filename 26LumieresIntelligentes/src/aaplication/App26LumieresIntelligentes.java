f5aa8238e03d7eee92b1e84425d2c1573aaa97f0
reinergayta <reinergayta@gmail.com>
2019-02-21 10:52:25 -0500

    Merge Reiner

--- 26LumieresIntelligentes/src/aaplication/App26LumieresIntelligentes.java
+++ 26LumieresIntelligentes/src/aaplication/App26LumieresIntelligentes.java
@@ -2,6 +2,7 @@ package aaplication;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.JFrame;
@@ -33,8 +34,97 @@ import java.awt.event.ActionEvent;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
 
 
 public class App26LumieresIntelligentes extends JFrame {
-	
+
+	private JPanel contentPane;
+	private java.net.URL  urlPhotoDeDepart = getClass().getClassLoader().getResource("photoDeDepart.jpg");
+	FenetreConcepts concepts;
+	FenetreInstructions instructions;
+	FenetreParametres parametres;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					App26LumieresIntelligentes frame = new App26LumieresIntelligentes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public App26LumieresIntelligentes() {
+		concepts = new FenetreConcepts();
+		instructions = new FenetreInstructions();
+		parametres = new FenetreParametres();
+		
+		setTitle("Lumi\u00E8res Intelligentes - Mamadou Barri, Reiner Luis Gayta\r\n");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 675, 543);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTitre = new JLabel("Lumi\u00E8res intelligentes");
+		lblTitre.setForeground(Color.YELLOW);
+		lblTitre.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblTitre.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitre.setBounds(39, 26, 568, 110);
+		contentPane.add(lblTitre);
+		
+		JButton btnDepart = new JButton("Commencer");
+		btnDepart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				parametres.setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnDepart.setBounds(248, 171, 171, 46);
+		contentPane.add(btnDepart);
+		
+		JButton btnInstructions = new JButton("Instructions");
+		btnInstructions.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				instructions.setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnInstructions.setBounds(248, 246, 171, 46);
+		contentPane.add(btnInstructions);
+		
+		JButton btnConceptsScientifiques = new JButton("Concepts Scientifiques");
+		btnConceptsScientifiques.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				concepts.setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnConceptsScientifiques.setBounds(248, 325, 171, 46);
+		contentPane.add(btnConceptsScientifiques);
+		
+		JButton btnQuitter = new JButton("Quitter");
+		btnQuitter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnQuitter.setBounds(248, 400, 171, 46);
+		contentPane.add(btnQuitter);
+		
+		JLabel lblphoto = new JLabel((new ImageIcon(urlPhotoDeDepart)));
+		lblphoto.setBounds(0, 0, 659, 504);
+		contentPane.add(lblphoto);
+	}
 }
--- 26LumieresIntelligentes/src/geometrie/Intersection.java
+++ 26LumieresIntelligentes/src/geometrie/Intersection.java
@@ -123,5 +123,7 @@ public class Intersection implements Dessinable {
 	public void setNbVoiesHorizontale(int nbVoiesHorizontale) {
 		this.nbVoiesHorizontale = nbVoiesHorizontale;
 	}
+	/*public int getX
+	*/
 	
 }
